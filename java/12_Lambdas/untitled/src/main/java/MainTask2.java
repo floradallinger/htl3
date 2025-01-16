f2f68daa24888e978484755dcfa41003fdc96bac
Flora Dallinger <flora.dallinger@gmx.at>
2025-01-16 14:17:11 +0100

    reducing the number with the avg

--- java/12_Lambdas/untitled/src/main/java/MainTask2.java
+++ java/12_Lambdas/untitled/src/main/java/MainTask2.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 
 
-public class Main {
+public class MainTask2 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println(numbers);
@@ -40,4 +40,5 @@ public class Main {
 interface MyFunction {
     Integer apply(Integer i);
 }
+
 
