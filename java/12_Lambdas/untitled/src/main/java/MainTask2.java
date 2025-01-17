188190ecc03cdc8375fe31faf90d8c45e80ec40d
Flora Dallinger <flora.dallinger@gmx.at>
2025-01-17 07:54:58 +0100

    Begun task 2. I have to filter numbers. Added some comments what the lambda should do

--- java/12_Lambdas/untitled/src/main/java/MainTask2.java
+++ java/12_Lambdas/untitled/src/main/java/MainTask2.java
@@ -1,44 +1,44 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
+import java.util.function.Predicate;
 
 public class MainTask2 {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println(numbers);
 
-        update(numbers, i -> i+1);
-        System.out.println(numbers);
-        update(numbers, i -> i*2);
-        System.out.println(numbers);
-        update(numbers, i -> i-2);
-        System.out.println(numbers);
-        update(numbers, i -> i*i);
-        System.out.println(numbers);
-        Integer sum = 0;
-        for (Integer num: numbers){
-            sum += num;
-        }
-        Integer avg = sum/numbers.size();
-        System.out.println(avg);
-        update(numbers, i -> i - avg);
-        System.out.println(numbers);
+        System.out.println(filterNumbers(numbers, i -> i % 2 == 0)); // gerade Zahlen
+        System.out.println(filterNumbers(numbers, i -> i % 2 != 0)); // ungerade Zahlen
+        System.out.println(filterNumbers(numbers, i -> i % 4 == 0)); // Vielfache von 4
+        System.out.println(filterNumbers(numbers, i -> { // Primzahlen
+            if(i == 0 || i == 1){
+                return false;
+            }
+            for (int j = 2; j < i; j++) {
+                if(i % j == 0){
+                    return false;
+                }
+            }
+            return true;
+        }));
+
 
 
     }
 
-    public static void update(List<Integer> numbers, MyFunction function) {
+    public static List<Integer> filterNumbers(List<Integer> numbers,  Predicate<Integer> condition) {
+        List<Integer> testedNumbers = new ArrayList();
         for (int i = 0; i < numbers.size(); i++) {
-            int oldValue = numbers.get(i);
-            int newValue = function.apply(oldValue);
-            numbers.set(i, newValue);
+            int valueToTest = numbers.get(i);
+            if(condition.test(valueToTest)){
+                testedNumbers.add(valueToTest);
+            }
         }
+        return testedNumbers;
     }
 
 }
 
-interface MyFunction {
-    Integer apply(Integer i);
-}
 
 
