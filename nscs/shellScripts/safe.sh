577dd2de237374e605ca87d660432bbda529e741
flora <flora.dallinger@gmx.at>
2025-01-28 09:31:51 +0100

    fixed a mistake in the shellskript since it didn't track the versions properly

--- nscs/shellScripts/safe.sh
+++ nscs/shellScripts/safe.sh
@@ -6,23 +6,32 @@ if test $# -gt 1; then
     exit
 fi
 
-if test $# != 1; then
-    verzeichnis="backup"S
-else
+verzeichnis="backup"
+
+if test $# = 1; then
     verzeichnis=$1
 fi
 
 
-if test ! -d $verzeichnis; then
-    mkdir $verzeichnis
-fi
+#if test ! -d $verzeichnis; then mkdir $verzeichnis fi
 
-time=$(expr $(date +%s) - $(date -d 2025-01-01 +%s))
+mkdir -p $verzeichnis
 
 for i in *.sh *.txt; do
     test $i = "*.sh" -o $i = "*.txt" && continue
+  
+    diffFile="$i.diff"
     if test ! -e "$verzeichnis/$i" || test $i -nt "$verzeichnis/$i"; then
         time=$(expr $(date +%s) - $(date -d 2025-01-01 +%s))
+        
+        version="---Version $time---"
+        echo $version >> $diffFile
+        if test ! -e "$verzeichnis/$i"; then
+            cat $i >> $diffFile
+        else
+            difference=$(diff "$verzeichnis/$i" $i)
+            echo $difference >> "$diffFile"
+        fi
         cp $i "$verzeichnis/$i.$time"
         cp $i "$verzeichnis/$i"
         echo "Kopiert"
