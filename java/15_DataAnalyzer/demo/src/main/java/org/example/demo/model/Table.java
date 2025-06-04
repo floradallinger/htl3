bd3adcdba8c8b5e8d553f1d150201bb675ef7f8f
floradallinger <flora.dallinger@gmx.at>
2025-06-04 11:26:03 +0200

    fix: only need one db

--- java/15_DataAnalyzer/demo/src/main/java/org/example/demo/model/Table.java
+++ java/15_DataAnalyzer/demo/src/main/java/org/example/demo/model/Table.java
@@ -1,5 +1,40 @@
 package org.example.demo.model;
 
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.util.ArrayList;
+
 public class Table {
+
+
+    public StringProperty tableName = new SimpleStringProperty();
+    public ObservableList<Column> columns = FXCollections.observableArrayList();
+
+    public Table(){}
+
+    public Table(String tableName){
+        setTableName(tableName);
+    }
+
+    public String toString(){
+        return "%s".formatted(
+                getTableName()
+        );
+    }
+
+
+    public StringProperty tableNameProperty() {
+        return tableName;
+    }
+
+    public void setTableName(String tableName) {
+        this.tableName.set(tableName);
+    }
+
+    public String getTableName() {
+        return tableName.get();
+    }
 
 }
