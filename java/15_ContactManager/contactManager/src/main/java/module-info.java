d59bd56573f91c7d3819fb9b4c5714d9f115948e
floradallinger <flora.dallinger@gmx.at>
2025-06-04 12:24:09 +0200

    refactor: deleted an unnessecary project

--- java/15_ContactManager/contactManager/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.contactmanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires jdk.jdi;
-
-
-    opens org.example.contactmanager to javafx.fxml;
-    exports org.example.contactmanager;
-}
